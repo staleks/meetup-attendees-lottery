b8a0ad4387bbd418bd12c5d803b2f67c0ec3ab55
Aleksandar <staleks@gmail.com>
2022-12-07 14:42:50 +0100

    added JavaDoc on controllers

--- src/main/java/rs/novacode/meetup/attendees/lottery/endpoint/MeetupAttendeeLotteryController.java
+++ src/main/java/rs/novacode/meetup/attendees/lottery/endpoint/MeetupAttendeeLotteryController.java
@@ -20,14 +20,25 @@ public class MeetupAttendeeLotteryController {
 
     private final String FETCH_ALL_MEETUP_ATTENDEES_ENDPOINT = "/attendees-all";
     private final String PICK_ONE_ATTENDEE_BY_LOTTERY = "/pick-one";
-
     private final AttendeesLotteryUseCase attendeesLotteryUseCase;
 
+    /**
+     * REST API method to fetch all Meetup Attendees in certain group.
+     * It is the list where Meetup Members opted for RSVP in a group.
+     * Fetched list populates in-memory data structure later used to Randomly pick one Meetup Member.
+     *
+     * @return {@link List} list of Meetup Members, attendees of certain Meetup Group
+     */
     @GetMapping(value = FETCH_ALL_MEETUP_ATTENDEES_ENDPOINT, produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<List<MeetupMember>> findAll() {
         return ok(attendeesLotteryUseCase.fetchAll());
     }
 
+    /**
+     * REST API method to pick one Randomly selected Meetup Member of certain group.
+     *
+     * @return {@link MeetupMember} lottery winner
+     */
     @GetMapping(value = PICK_ONE_ATTENDEE_BY_LOTTERY, produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<MeetupMember> pickOne() {
         return ok(attendeesLotteryUseCase.pickOneByLottery());
